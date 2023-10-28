bfd73a20c7fff0a4021effdaf9f267dec177f8fd
João Vitor <joaovitorsantoscs@gmail.com>
2023-10-27 22:30:03 -0300

    :sparkles: write to csv feature added but WIP

--- kabum_scrapy/pipelines.py
+++ kabum_scrapy/pipelines.py
@@ -1,13 +1,14 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
-
 class KabumScrapyPipeline:
     def process_item(self, item, spider):
+        return item
+    
+class WriteCSV:
+    def open_spider(self, spider):
+        self.file = open('kabum.csv', 'w', encoding='utf-8')
+    def close_spider(self, spider):
+        self.file.close()
+    def process_item(self, item, spider):
+        line = f'{item["name"]};{item["price"]};{item["url"]};{item["last_update"]}\n'
+        self.file.write(line)
         return item
